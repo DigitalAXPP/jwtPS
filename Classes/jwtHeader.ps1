5552beeca5c163da92bb964cbec0aec4519651c8
DigitalAXPP <a.piepenhagen@gmx.net>
2020-12-19 00:23:06 +0100

    Updating the classes

--- Classes/jwtHeader.ps1
+++ Classes/jwtHeader.ps1
@@ -2,8 +2,11 @@ class jwtHeader {
     [Algorithm]$Algorithm
     [string]$TokenType = "JWT"
 
-    [string]CreateHeader(){
-        $headerContent = "{ 'alg': $($this.Algorithm), 'typ': $($this.TokenType)}" | ConvertTo-Json
+    [string]Create(){
+        $headerContent = @{ 
+            'alg'= $($this.Algorithm)
+            'typ'= $($this.TokenType)
+        } | ConvertTo-Json
         $base64 = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($headerContent))
         return $base64 -replace "="
     }
--- Classes/jwtSignature.ps1
+++ Classes/jwtSignature.ps1
@@ -8,13 +8,23 @@ class jwtSignature {
     }
     
     [string]Create() {
-        Set-Content -Path $env:TEMP\key.pem -Value $this.PrivateKey
-        Set-Content -Path $env:TEMP\data.txt -Value $this.Data -NoNewline
+        $rsa_Base64 = [string]::Empty
+        try {
+            Set-Content -Path $env:TEMP\key.pem -Value $this.PrivateKey
+            Set-Content -Path $env:TEMP\data.txt -Value $this.Data -NoNewline
 
-        openssl dgst -sha256 -sign $env:TEMP\PrivateKey.pem -out $env:TEMP\sig.txt $env:TEMP\data.txt
+            openssl dgst -sha256 -sign $env:TEMP\PrivateKey.pem -out $env:TEMP\sig.txt $env:TEMP\data.txt
 
-        $rsa_signature = [System.IO.File]::ReadAllBytes("$env:TEMP\sig.txt")
-        $rsa_Base64 = [Convert]::ToBase64String($rsa_signature) -replace '\+','-' -replace '/','_' -replace '='
+            $rsa_signature = [System.IO.File]::ReadAllBytes("$env:TEMP\sig.txt")
+            $rsa_Base64 = [Convert]::ToBase64String($rsa_signature) -replace '\+','-' -replace '/','_' -replace '='
+        }
+        catch {
+            throw [System.IO.IOException]::new($_.Exception.Message)
+        }
+        finally {
+            Remove-Item -Path $env:TEMP\key.pem
+            Remove-Item -Path $env:TEMP\data.txt
+        }
         return $rsa_Base64
     }
 }
