aadb8c39c590fca1bedee7c375c8be756ba74575
DigitalAXPP <a.piepenhagen@gmx.net>
2021-01-04 23:49:33 +0100

    Adding Switch statement to allow different algorithm lengths.

--- Classes/jwtSignature.ps1
+++ Classes/jwtSignature.ps1
@@ -1,6 +1,7 @@
 class jwtSignature {
     [string]$PrivateKey
     [string]$Data
+    [Algorithm]$Algorithm
 
     jwtSignature ([string]$key, [string]$data) {
         $this.PrivateKey = $key
@@ -13,7 +14,20 @@ class jwtSignature {
             Set-Content -Path $env:TEMP\key.pem -Value $this.PrivateKey
             Set-Content -Path $env:TEMP\data.txt -Value $this.Data -NoNewline
 
-            openssl dgst -sha256 -sign "$env:TEMP\key.pem" -out "$env:TEMP\sig.txt" "$env:TEMP\data.txt"
+            switch ($this.Algorithm -replace "[A-Z]") {
+                256 {  
+                    openssl dgst -sha256 -sign "$env:TEMP\key.pem" -out "$env:TEMP\sig.txt" "$env:TEMP\data.txt"
+                }
+                284 {
+                    openssl dgst -sha384 -sign "$env:TEMP\key.pem" -out "$env:TEMP\sig.txt" "$env:TEMP\data.txt"
+                }
+                512 {
+                    openssl dgst -sha512 -sign "$env:TEMP\key.pem" -out "$env:TEMP\sig.txt" "$env:TEMP\data.txt"
+                }
+                Default {
+                    throw [System.ArgumentException]::new("Unavailable Algorithm length.")
+                }
+            }
 
             $rsa_signature = [System.IO.File]::ReadAllBytes("$env:TEMP\sig.txt")
             $rsa_Base64 = [Convert]::ToBase64String($rsa_signature)
