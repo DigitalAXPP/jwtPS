1b350903b8edbc8316ea01ab55cc9d4d00d731e4
DigitalAXPP <a.piepenhagen@gmx.net>
2021-01-15 23:40:10 +0100

    New function 'ConvertFrom-JWT'

---
+++ Functions/ConvertFrom-JWT.ps1
@@ -0,0 +1,25 @@
+function ConvertFrom-JWT {
+    [CmdletBinding()]
+    param (
+        [Parameter(
+            HelpMessage='Enter the JWT you want to convert to human readable text.'
+        )]
+        [string]$JWT
+    )
+    
+    begin {
+        
+    }
+    
+    process {
+        $header, $payload, $signature = $JWT.Split('.')
+        $reversedJWT = [PSCustomObject]@{
+            'Header' = [System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($header))
+            'Payload' = [System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($payload))
+        }
+    }
+    
+    end {
+        Write-Output -InputObject $reversedJWT
+    }
+}
