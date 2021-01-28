d0ad65c16c9f26b58b69ebc9443fce3071213878
DigitalAXPP <a.piepenhagen@gmx.net>
2021-01-28 23:30:33 +0100

    function skeleton for Test-JWT

---
+++ Functions/Test-JWT.ps1
@@ -0,0 +1,22 @@
+function Test-JWT {
+    [CmdletBinding()]
+    param (
+        [Parameter(
+            Mandatory,
+            HelpMessage='Enter the JWT.'
+        )]
+        [string]$JWT
+    )
+    
+    begin {
+        
+    }
+    
+    process {
+        
+    }
+    
+    end {
+        
+    }
+}
