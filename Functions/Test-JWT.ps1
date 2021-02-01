c62307acdbb37aa05c95a3e086902291d32822ec
DigitalAXPP <a.piepenhagen@gmx.net>
2021-02-02 00:23:25 +0100

    Extending the test-jwt function to verify all available RS algorithms.

--- Functions/Test-JWT.ps1
+++ Functions/Test-JWT.ps1
@@ -25,12 +25,27 @@ function Test-JWT {
             $header, $payload, $signature = $JWT.Split(".")
             $preparedSignature = $signature.Insert(($signature.Length), "==").Replace('-', '+').Replace('_', '/')
             $bytes = [System.Convert]::FromBase64String($preparedSignature)
+            $headerDecoded = [System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($header)) | ConvertFrom-Json
             #endregion
             Set-Content -Path $env:TEMP\data.txt -Value "$header.$payload" -NoNewline
             Set-Content -Path $env:TEMP\sig.txt -Value $bytes -AsByteStream
     
             #region Verify signature
-            $result = openssl dgst -verify $PublicKey -signature $env:TEMP\sig.txt $env:TEMP\data.txt
+            switch ($headerDecoded.alg) {
+                'RS256' {  
+                    $result = openssl dgst -sha256 -verify $PublicKey -signature $env:TEMP\sig.txt $env:TEMP\data.txt
+                }
+                'RS384' {  
+                    $result = openssl dgst -sha384 -verify $PublicKey -signature $env:TEMP\sig.txt $env:TEMP\data.txt
+                }
+                'RS512' {  
+                    $result = openssl dgst -sha512 -verify $PublicKey -signature $env:TEMP\sig.txt $env:TEMP\data.txt
+                }
+                Default {
+                    throw [System.ArgumentOutOfRangeException]::new("The JWT uses an unsupported algorithm.")
+                }
+            }
+            
             #endregion            
         }
         catch [System.Management.Automation.MethodException] {
