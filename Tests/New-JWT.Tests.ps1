ac6a2f79253952887093e13d8b6173166288f4d2
DigitalAXPP <a.piepenhagen@gmx.net>
2021-02-10 23:13:03 +0100

    Completing tests for New-JWT

--- Tests/New-JWT.Tests.ps1
+++ Tests/New-JWT.Tests.ps1
@@ -34,5 +34,34 @@ Describe "New-JWT" {
             $jwt = New-JWT -PrivateKey $key.PrivateKey -Algorithm RS384 -Payload $claim
             $jwt | Should -Match -RegularExpression '(^[\w-]*\.[\w-]*\.[\w-]*$)'
         }
+        It "With SHA512" {
+            $jwt = New-JWT -PrivateKey $key.PrivateKey -Algorithm RS512 -Payload $claim
+            $jwt | Should -Match -RegularExpression '(^[\w-]*\.[\w-]*\.[\w-]*$)'
+        }
+    }
+    Context "Creating HMAC signature" {
+        BeforeEach {
+            $claim = @{
+                aud = "jwtPS"        
+                iss = "DigitalAXPP"        
+                sub = "HS256 Test"        
+                nbf = "0"        
+                exp = ([System.DateTimeOffset]::Now.AddHours(3)).ToUnixTimeSeconds()
+                iat = ([System.DateTimeOffset]::Now).ToUnixTimeSeconds()
+                jti = [guid]::NewGuid()
+            }
+        }
+        It "With SHA256" {
+            $jwt = New-JWT -Algorithm HS256 -Payload $claim -PrivateKey 'P@ssw0rd'
+            $jwt | Should -Match -RegularExpression '(^[\w-]*\.[\w-]*\.[\w-]*$)'
+        }
+        It "With SHA384" {
+            $jwt = New-JWT -Algorithm HS384 -Payload $claim -PrivateKey 'P@ssw0rd'
+            $jwt | Should -Match -RegularExpression '(^[\w-]*\.[\w-]*\.[\w-]*$)'
+        }
+        It "With SHA512" {
+            $jwt = New-JWT -Algorithm HS512 -Payload $claim -PrivateKey 'P@ssw0rd'
+            $jwt | Should -Match -RegularExpression '(^[\w-]*\.[\w-]*\.[\w-]*$)'
+        }
     }
 }
