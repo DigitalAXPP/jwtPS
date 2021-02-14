d6442b45afffda215e2818b091cc14770fe57381
DigitalAXPP <a.piepenhagen@gmx.net>
2021-02-14 23:58:40 +0100

    First RSA test block

--- Tests/Test-JWT.Tests.ps1
+++ Tests/Test-JWT.Tests.ps1
@@ -27,4 +27,23 @@ Describe "Test-JWT" {
             $command.Parameters[$parameter].ParameterSets[$parameterset].IsMandatory | Should -BeTrue
         }
     }
+    BeforeEach {
+        $key = Get-Content -Path $env:HOMEPATH\Documents\PowerShell\rsa2048.json | ConvertFrom-Json
+        $claim = @{
+            aud = "jwtPS"        
+            iss = "DigitalAXPP"        
+            sub = "RS256 Test"        
+            nbf = "0"        
+            exp = ([System.DateTimeOffset]::Now.AddHours(3)).ToUnixTimeSeconds()
+            iat = ([System.DateTimeOffset]::Now).ToUnixTimeSeconds()
+            jti = [guid]::NewGuid()
+        }
+    }
+    Context "RSA encryption" {
+        It "with SHA 256" {
+            $jwt = New-JWT -PrivateKey $key.PrivateKey -Algorithm RS256 -Payload $claim
+            $result = Test-JWT -JWT $jwt -PublicKey $key.PublicKey
+            $result | Should -Contain "OK"
+        }
+    }
 }
