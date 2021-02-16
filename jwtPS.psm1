44d8f62d7170e94bdfb91d2a658001e8d43061e4
DigitalAXPP <a.piepenhagen@gmx.net>
2021-02-16 20:57:42 +0100

    Updating .psm1 file

--- jwtPS.psm1
+++ jwtPS.psm1
@@ -1,6 +1,6 @@
 # .ExternalHelp jwtPS-help.xml
 
-$files = Get-ChildItem -Path $PSScriptRoot -Filter *.ps1 -Recurse -File -Force
+$files = Get-ChildItem -Path $PSScriptRoot -Filter *.ps1 -Exclude *.Tests.ps1 -Recurse -File -Force
 
 foreach ($file in $files) {
     . $file.FullName
