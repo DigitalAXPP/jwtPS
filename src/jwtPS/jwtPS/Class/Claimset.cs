900db2d5901447d08659fdbc8d50d36a0517bace
DigitalAXPP <a.piepenhagen@gmx.net>
2021-03-13 23:58:36 +0100

    Adding class claimset and signature

---
+++ src/jwtPS/jwtPS/Class/Claimset.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Text;
+
+namespace jwtPS.Class
+{
+    public class Claimset
+    {
+        public string Create(Hashtable Payload)
+        {
+            var json = JsonConvert.SerializeObject(Payload);
+            var bytes = Encoding.UTF8.GetBytes(json);
+            return Convert.ToBase64String(bytes);
+        }
+    }
+}
---
+++ src/jwtPS/jwtPS/Class/Signature.cs
@@ -0,0 +1,54 @@
+﻿using JWT.Algorithms;
+using JWT.Builder;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace jwtPS.Class
+{
+    public class Signature
+    {
+        public List<KeyValuePair<string, object>> ClaimSet { get; set; }
+        public string Algorithm { get; set; }
+
+        public Signature(List<KeyValuePair<string, object>> Data, string Algorithm)
+        {
+            ClaimSet = Data;
+            this.Algorithm = Algorithm;
+        }
+
+        public string Create(RSA PrivateKey, RSA PublicKey)
+        {
+            string token = null;
+            switch (Algorithm)
+            {
+                case "ES256":
+                    {
+
+
+                    }
+                    break;
+                case "RS256":
+                    {
+                        token = JwtBuilder.Create()
+                                          .WithAlgorithm(new RS256Algorithm(PublicKey, PrivateKey))
+                                          .AddClaims(ClaimSet)
+                                          .MustVerifySignature()
+                                          .Encode();
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return token;
+        }
+        public string Create(string Secret)
+        {
+            var token = JwtBuilder.Create()
+                                  .WithAlgorithm(new HMACSHA256Algorithm())
+                                  .WithSecret(Secret)
+                                  .AddClaims(ClaimSet)
+                                  .Encode();
+            return token;
+        }
+    }
+}
