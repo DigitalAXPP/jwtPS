b61739c997c8541f4d618eff089a99811a62e9ee
DigitalAXPP <a.piepenhagen@gmx.net>
2021-03-13 23:42:45 +0100

    Moved files to separate folders

---
+++ src/jwtPS/jwtPS/Class/Header.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jwtPS.Class
+{
+    public class Header
+    {
+        const string typ = "JWT";
+        private string alg { get; set; }
+
+        public Header(string Algorithm)
+        {
+            alg = Algorithm;
+        }
+
+        public string Create()
+        {
+            var dict = new Dictionary<string, string>()
+            {
+                { "typ", typ },
+                { "alg", alg }
+            };
+            var json = JsonConvert.SerializeObject(dict);
+            var bytes = Encoding.UTF8.GetBytes(json);
+            return Convert.ToBase64String(bytes);
+        }
+    }
+}
