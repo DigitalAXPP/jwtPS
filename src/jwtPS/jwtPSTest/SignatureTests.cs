8c42550057c5ff4e9ba7b76bf6aac888e6bc77d7
DigitalAXPP <a.piepenhagen@gmx.net>
2021-03-14 23:07:37 +0100

    Complete constructor test for signature class.

--- src/jwtPS/jwtPSTest/SignatureTests.cs
+++ src/jwtPS/jwtPSTest/SignatureTests.cs
@@ -1,4 +1,7 @@
-﻿using Xunit;
+﻿using jwtPS.Class;
+using System;
+using System.Collections.Generic;
+using Xunit;
 
 namespace jwtPSTest
 {
@@ -7,7 +10,22 @@ namespace jwtPSTest
         [Fact]
         public void ConstructorTest()
         {
+            //-- Arrange
+            var payload = new List<KeyValuePair<string, object>>()
+            {
+                new KeyValuePair<string, object>( "aud", "jwtPS" ),
+                new KeyValuePair<string, object>( "iss", "DigitalAXPP" ),
+                new KeyValuePair<string, object>( "sub", "RS256 Test" ),
+                new KeyValuePair<string, object>( "nbf", "0" ),
+                new KeyValuePair<string, object>( "exp", DateTime.Today.AddDays(1))
+            };
 
+            //-- Act
+            var signature = new Signature(payload, "HS256");
+
+            //-- Assert
+            Assert.IsType<Signature>(signature);
+            Assert.Equal("HS256", signature.Algorithm);
         }
     }
 }
