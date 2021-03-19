1dc2d9cd62281e8d0cdf19f4601a0c92a298595f
DigitalAXPP <a.piepenhagen@gmx.net>
2021-03-19 23:15:42 +0100

    Adding Algorithm enum

---
+++ src/jwtPS/jwtPS/Enum/Algorithm.cs
@@ -0,0 +1,15 @@
+﻿namespace jwtPS.Enum
+{
+    public enum Algorithm
+    {
+        HS256,
+        HS384,
+        HS512,
+        RS256,
+        RS483,
+        RS512,
+        ES256,
+        ES384,
+        ES512
+    }
+}
