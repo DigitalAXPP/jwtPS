36dc2bc75e093897e75fa15a3cd9941da01246ab
DigitalAXPP <a.piepenhagen@gmx.net>
2021-03-24 23:40:25 +0100

    Starting to create a new static ToRSA method to convert a string to an RSA class.

--- src/jwtPS/jwtPS/Cmdlet/NewJWTCmdlet.cs
+++ src/jwtPS/jwtPS/Cmdlet/NewJWTCmdlet.cs
@@ -1,5 +1,6 @@
 ﻿using jwtPS.Enum;
-using System.Collections.Generic;
+using jwtPS.Extension;
+using System.Collections;
 using System.Management.Automation;
 using Signature = jwtPS.Class.Signature;
 
@@ -12,22 +13,39 @@ namespace jwtPS
         public string Privatekey { get; set; }
         [Parameter(HelpMessage = "Enter the public key")]
         public string Publickey { get; set; }
+        [Parameter(HelpMessage = "Enter the HMAC secret.")]
+        public string Secret { get; set; }
         [Parameter(HelpMessage = "Enter the desired algorithm for the JWT.")]
         public Algorithm Algorithm { get; set; }
         [Parameter(HelpMessage = "Enter the payload.")]
-        public Dictionary<string, object> Payload { get; set; }
+        public Hashtable Payload { get; set; }
         protected override void ProcessRecord()
         {
             base.ProcessRecord();
             //var header = new Header(Algorithm).Create();
             //var claimset = new Claimset().Create(Payload);
-            var jwt = new Signature(Payload, Algorithm);
-            //switch (ParameterSetName)
-            //{
-            //    default:
-            //        break;
-            //}
-            WriteObject("Test");
+            var claim = Conversion.ToDictionary<string, object>(Payload);
+            var jwt = new Signature(claim, Algorithm);
+            string token = null;
+            switch (Algorithm)
+            {
+                case Algorithm.HS256:
+                case Algorithm.HS384:
+                case Algorithm.HS512:
+                    {
+                        token = jwt.Create(Secret);
+                    }
+                    break;
+                case Algorithm.RS256:
+                case Algorithm.RS384:
+                case Algorithm.RS512:
+                    {
+                        token = jwt.Create(Publickey, Privatekey);
+                    }
+                default:
+                    break;
+            }
+            WriteObject(claim);
         }
     }
 }
--- src/jwtPS/jwtPS/Extension/Conversion.cs
+++ src/jwtPS/jwtPS/Extension/Conversion.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 
 namespace jwtPS.Extension
 {
@@ -9,9 +11,14 @@ namespace jwtPS.Extension
         public static Dictionary<K, V> ToDictionary<K, V>(this Hashtable Table)
         {
             return Table
-
                 .Cast<DictionaryEntry>()
                 .ToDictionary(kvp => (K)kvp.Key, kvp => (V)kvp.Value);
+        }
+        public static RSA ToRSA(this string Key)
+        {
+            var bytes = Convert.FromBase64String(Key);
+            using var rsa = RSA.Create();
+            rsa.ImportPkcs8PrivateKey(bytes, out _);
         }
     }
 }
