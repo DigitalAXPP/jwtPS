78189690ca96be391bc150c5aa48ff28c6cac5b2
DigitalAXPP <a.piepenhagen@gmx.net>
2021-03-30 00:24:21 +0200

    XUnit test for the PSCmdlet class

--- src/jwtPS/jwtPS/Cmdlet/NewJWTCmdlet.cs
+++ src/jwtPS/jwtPS/Cmdlet/NewJWTCmdlet.cs
@@ -48,7 +48,7 @@ namespace jwtPS
                 default:
                     break;
             }
-            WriteObject(claim);
+            WriteObject(token);
         }
     }
 }
---
+++ src/jwtPS/jwtPSTest/NewJWTCmdletTests.cs
@@ -0,0 +1,36 @@
+﻿using jwtPS;
+using jwtPS.Enum;
+using System;
+using System.Collections;
+using Xunit;
+
+namespace jwtPSTest
+{
+    public class NewJWTCmdletTests
+    {
+        [Fact]
+        public void HMACTests()
+        {
+            //-- Arrange
+            var secret = "GQDstcKsx0NHjPOuXOYg5MbeJ1XT0uFiwDVvVBrk";
+            var claim = new Hashtable()
+            {
+                { "name", "Alex" },
+                { "age", 21},
+                { "date", DateTimeOffset.Now.AddHours(3).ToUnixTimeSeconds() }
+            };
+            var cmdlet = new NewJWTCmdlet()
+            {
+                Secret = secret,
+                Payload = claim,
+                Algorithm = Algorithm.HS256
+            };
+
+            //-- Act
+            var result = cmdlet.Invoke();
+
+            //-- Assert
+            Assert.NotNull(result);
+        }
+    }
+}
