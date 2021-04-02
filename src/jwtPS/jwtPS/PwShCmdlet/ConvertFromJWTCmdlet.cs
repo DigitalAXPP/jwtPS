dff32542d28c968f79716cf31904979a28ca4a8b
DigitalAXPP <a.piepenhagen@gmx.net>
2021-04-03 00:05:15 +0200

    Preparing ConvertFrom-JWT

--- src/jwtPS/jwtPS/PwShCmdlet/ConvertFromJWTCmdlet.cs
+++ src/jwtPS/jwtPS/PwShCmdlet/ConvertFromJWTCmdlet.cs
@@ -1,9 +1,25 @@
-﻿using System.Management.Automation;
+﻿using System;
+using System.Collections;
+using System.Management.Automation;
 
 namespace jwtPS.PwShCmdlet
 {
     [Cmdlet(VerbsData.ConvertFrom, "JWT")]
     public class ConvertFromJWTCmdlet : Cmdlet
     {
+        [Parameter(HelpMessage = "Enter a valid JWT token.")]
+        public string JWT { get; set; }
+        protected override void ProcessRecord()
+        {
+            base.ProcessRecord();
+
+            var jwtParts = JWT.Split('.');
+            var table = new Hashtable()
+            {
+                { "Header", Convert.FromBase64String(jwtParts[0]) },
+                { "Payload", Convert.FromBase64String(jwtParts[1]) }
+            };
+            WriteObject(table);
+        }
     }
 }
