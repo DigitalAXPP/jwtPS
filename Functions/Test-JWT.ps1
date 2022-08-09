17f45d5752971cafb6267d75f81a7e819ae692f2
LAJAUNIE Richard <richard.lajaunie@cote-azur.aeroport.fr>
2022-08-09 14:28:03 +0200

    Change file handling for linux platform and had expiration date test

--- Functions/Test-JWT.ps1
+++ Functions/Test-JWT.ps1
@@ -34,37 +34,44 @@ function Test-JWT {
     }
 
     process {
+        $tmpPath = [IO.Path]::GetTempPath()
+        $FullPathToData = $tmpPath + "data.txt"
+        $FullPathToSig  = $tmpPath + "sig.txt"
+        $PublicKey = [System.IO.Path]::GetFullPath("$PublicKey")
+
         try {
             #region Reversing and splitting the JWT
             $header, $payload, $signature = $JWT.Split(".")
+
             $bytes = ConvertFrom-Base64 -Base64 $signature -Byte
             $headerDecoded = ConvertFrom-Base64 -Base64 $header
             #endregion
-            Set-Content -Path $env:TEMP\data.txt -Value "$header.$payload" -NoNewline
-            Set-Content -Path $env:TEMP\sig.txt -Value $bytes -AsByteStream
+
+            Set-Content -Path $FullPathToData -Value "$header.$payload" -NoNewline
+            Set-Content -Path $FullPathToSig -Value $bytes -AsByteStream
 
             #region Verify signature
             switch ($headerDecoded.alg) {
                 { $_ -in @('RS256', 'ES256') } {
-                    $result = openssl dgst -sha256 -verify $PublicKey -signature $env:TEMP\sig.txt $env:TEMP\data.txt
+                    $result = openssl dgst -sha256 -verify $PublicKey -signature $FullPathToSig $FullPathToData
                 }
                 { $_ -in @('RS384', 'ES384') } {
-                    $result = openssl dgst -sha384 -verify $PublicKey -signature $env:TEMP\sig.txt $env:TEMP\data.txt
+                    $result = openssl dgst -sha384 -verify $PublicKey -signature $FullPathToSig $FullPathToData
                 }
                 { $_ -in @('RS512', 'ES512') } {
-                    $result = openssl dgst -sha512 -verify $PublicKey -signature $env:TEMP\sig.txt $env:TEMP\data.txt
+                    $result = openssl dgst -sha512 -verify $PublicKey -signature $FullPathToSig $FullPathToData
                 }
                 'HS256' {
-                    Remove-Item -Path $env:TEMP\sig.txt -Force
-                    $result = openssl dgst -sha256 -mac HMAC -macopt key:$Secret -out "$env:TEMP\sig.txt" "$env:TEMP\data.txt"
+                    Remove-Item -Path $FullPathToSig -Force
+                    $result = openssl dgst -sha256 -mac HMAC -macopt key:$Secret -out $FullPathToSig $FullPathToData
                 }
                 'HS384' {
-                    Remove-Item -Path $env:TEMP\sig.txt -Force
-                    $result = openssl dgst -sha384 -mac HMAC -macopt key:$Secret -out "$env:TEMP\sig.txt" "$env:TEMP\data.txt"
+                    Remove-Item -Path $FullPathToSig -Force
+                    $result = openssl dgst -sha384 -mac HMAC -macopt key:$Secret -out $FullPathToSig $FullPathToData
                 }
                 'HS512' {
-                    Remove-Item -Path $env:TEMP\sig.txt -Force
-                    $result = openssl dgst -sha512 -mac HMAC -macopt key:$Secret -out "$env:TEMP\sig.txt" "$env:TEMP\data.txt"
+                    Remove-Item -Path $FullPathToSig -Force
+                    $result = openssl dgst -sha512 -mac HMAC -macopt key:$Secret -out $FullPathToSig $FullPathToData
                 }
                 Default {
                     throw [System.ArgumentOutOfRangeException]::new("The JWT uses an unsupported algorithm.")
@@ -72,18 +79,27 @@ function Test-JWT {
             }
             #endregion
             if ($PSBoundParameters.ContainsKey('Secret')) {
-                Get-Content -Path $env:TEMP\sig.txt | Where-Object { $_ -match '(?<=\= )\w*$' }
+                Get-Content -Path $FullPathToSig | Where-Object { $_ -match '(?<=\= )\w*$' }
                 $bytes = [System.Text.Encoding]::UTF8.GetBytes($Matches[0])
                 $rsa_Base64 = [System.Convert]::ToBase64String($bytes)
                 $result = $preparedSignature -eq $rsa_Base64
             }
+
+            # Is Token Expired
+            $jwtDatas = ConvertFrom-JWT($JWT)
+            $PayloadExpirationDate = Get-Date -UnixTimeSeconds $jwtDatas.Payload.exp
+            $now = (Get-Date).ToUniversalTime()
+            if ($now -gt $PayloadExpirationDate) {
+                $result = "Expired"
+            }
+
         }
         catch [System.Management.Automation.MethodException] {
             throw [System.Management.Automation.MethodException]::new($_.Exception.Message)
         }
         finally {
-            Remove-Item -Path $env:TEMP\data.txt -Force
-            Remove-Item -Path $env:TEMP\sig.txt -Force
+            Remove-Item -Path $FullPathToData -Force
+            Remove-Item -Path $FullPathToSig -Force
         }
     }
 
