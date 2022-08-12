4a4d513063fd2240a0821febf9d019ec47971959
LAJAUNIE Richard <richard.lajaunie@cote-azur.aeroport.fr>
2022-08-12 17:32:30 +0200

    had -Compress to ConvertTo-Json

--- Classes/jwtHeader.ps1
+++ Classes/jwtHeader.ps1
@@ -3,9 +3,9 @@ class jwtHeader : jwtBase {
 
     [string]Create(){
         $headerContent = @{
-            'alg'= $this.Algorithm
             'typ'= $this.TokenType
-        } | ConvertTo-Json -EnumsAsStrings
+            'alg'= $this.Algorithm
+        } | ConvertTo-Json -EnumsAsStrings -Compress
         $base64 = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($headerContent)) -replace '\+','-' -replace '/','_' -replace '='
         return $base64
     }
